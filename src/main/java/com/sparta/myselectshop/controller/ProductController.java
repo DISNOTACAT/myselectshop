8958fcf1c5f0a6b805ab9f9009b93a01dba3ee10
DISNOTACAY <yeonjh915@gmail.com>
2024-11-05 11:19:25 +0900

    admin 추가 완료

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +19,28 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-        return productService.createProduct(requestDto);
-    }
-
     @PutMapping("/products/{id}")
-   public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         return productService.updateProduct(id, requestDto);
     }
 
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 응답 보내기
+        return productService.createProduct(requestDto, userDetails.getUser());
+    }
+
+    // 관심 상품 조회하기
     @GetMapping("/products")
+    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 응답 보내기
+        return productService.getProducts(userDetails.getUser());
+    }
+
+    // 관리자 조회
+    @GetMapping("/admin/products")
     public List<ProductResponseDto> getAllProducts() {
-        return productService.getProducts();
+        return productService.getAllProducts();
     }
 }
